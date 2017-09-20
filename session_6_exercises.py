3e5d89439b8216d7afd420c9c6db04d36135b0bb
Bedrock123 <info@builtbykingwilly.com>
2017-09-20 13:40:45 -0400

    Completed excersies 1-4

--- session_6_exercises.py
+++ session_6_exercises.py
@@ -5,6 +5,61 @@ Session 6 Exercises
 """
 
 """
-Write an appropriately general set of functions that can draw shapes as below.
+1. Write a program, factorial.py to compute a factorial of an integer, n.
 """
+def factorial(n):
+	"""
+	calculates the factorial of the n parameter
+	and uses recurstion to find the 
+	"""
+	if n == 0:
+		return 1
+	else:
+		return n * factorial(n-1)
 
+# Run Function
+print(factorial(30))
+
+
+"""
+2. Write a program, fibonacci.py to compute the Fibonacci number of an integer , n.
+"""
+def fibonacci(n):
+	"""
+	calculates the fibonacci sequence of the n parameter
+	and uses recurstion to find the 
+	"""
+	if n == 0: 
+		return 0
+	elif n == 1:
+		return 1
+	else: 
+		return fibonacci(n-1)+fibonacci(n-2)
+
+# Run Function
+print(fibonacci(10))
+
+def gcd(x, y):
+	"""
+	calculates the greatest common divisor of the x and y parameters
+	reference: https://en.wikipedia.org/wiki/Euclidean_algorithm#Worked_example
+	Uses recursion
+	"""
+	if y == 0:
+		return x; 
+	else:
+		return gcd(y, x%y)
+
+# Run Function
+print(gcd(2,12))
+
+"""
+4. Implement algorithm for tower of Hanoi. https://www.mathsisfun.com/games/towerofhanoi.html
+"""
+def move(n, source, bridge, destination):
+	if n > 0:
+		move(n-1, source, destination, bridge)
+		print(source, ' --> ', bridge)
+		move(n-1, destination, bridge, source)
+
+print(move(3, 'A', 'B', 'C'))
